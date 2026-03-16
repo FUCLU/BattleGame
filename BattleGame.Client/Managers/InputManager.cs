e727c25a90e83d02a516f44397a724cbb83cc61b
Phan Hưng <phanthaihungwork@gmail.com>
2026-03-17 01:22:46 +0700

    thaydoi cautruc

--- BattleGame.Client/Managers/InputManager.cs
+++ BattleGame.Client/Managers/InputManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BattleGame.Client.Game
+namespace BattleGame.Client.Managers
 {
     internal class InputManager
     {
---
+++ BattleGame.Client/Network/MatchmakingClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleGame.Client.Network
+{
+    internal class MatchmakingClient
+    {
+    }
+}
