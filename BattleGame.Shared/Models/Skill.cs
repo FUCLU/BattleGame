7411edd568edecb00b0667ff1b56ed3df9772237
Phan Hưng <phanthaihungwork@gmail.com>
2026-03-17 01:22:46 +0700

    thaydoi cautruc

--- BattleGame.Shared/Models/Skill.cs
+++ BattleGame.Shared/Models/Skill.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BattleGame.Client.Game
+namespace BattleGame.Shared.Models
 {
-    internal class MatchmakingClient
+    internal class Skill
     {
     }
 }
