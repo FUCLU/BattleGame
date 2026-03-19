cf1c41b21cf279f31c1c1793f0c43647ae05d6f9
FUCLU <24521382@gm.uit.edu.vn>
2026-03-19 10:09:44 +0700

    update repo

---
+++ BattleGame.Server/Logging/ServerLogger.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleGame.Server.Logging
+{
+    internal class ServerLogger
+    {
+    }
+}
--- BattleGame.Server/Program.cs
+++ BattleGame.Server/Program.cs
@@ -1,3 +1,16 @@
 ﻿using BattleGame.Server.Network;
+using BattleGame.Server.Config;
 
-Console.WriteLine("=== SERVER MODE ===");
+Console.WriteLine("=== BATTLEGAME SERVER STARTING ===");
+
+var config = ServerConfig.Load();
+var server = new GameServer(config.Port);
+
+Console.CancelKeyPress += (_, e) => {
+    e.Cancel = true;
+    Console.WriteLine("[INFO] Shutting down...");
+    server.Stop();
+};
+
+Console.WriteLine($"[INFO] Server listening on port {config.Port}");
+server.Start();
