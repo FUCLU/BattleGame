728745740e57ac8ef63232fb69010a8331048901
Nguyen Tan Phat <tanphatnguyen.tp@gmail.com>
2026-03-19 15:20:53 +0700

    test

--- BattleGame.Client/Forms/LoginForm.Designer.cs
+++ BattleGame.Client/Forms/LoginForm.Designer.cs
@@ -28,12 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginForm";
+            pictureBox1 = new PictureBox();
+            pnlLogin = new Panel();
+            btnRegister = new Button();
+            btnLogin = new Button();
+            checkShowpassword = new CheckBox();
+            txtPassword = new TextBox();
+            pictureBox4 = new PictureBox();
+            panel3 = new Panel();
+            txtUsername = new TextBox();
+            pictureBox5 = new PictureBox();
+            panel4 = new Panel();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            pnlLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.backround_login;
+            pictureBox1.Location = new Point(-49, -26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1081, 630);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pnlLogin
+            // 
+            pnlLogin.BackColor = Color.Gray;
+            pnlLogin.Controls.Add(btnRegister);
+            pnlLogin.Controls.Add(btnLogin);
+            pnlLogin.Controls.Add(checkShowpassword);
+            pnlLogin.Controls.Add(txtPassword);
+            pnlLogin.Controls.Add(pictureBox4);
+            pnlLogin.Controls.Add(panel3);
+            pnlLogin.Controls.Add(txtUsername);
+            pnlLogin.Controls.Add(pictureBox5);
+            pnlLogin.Controls.Add(panel4);
+            pnlLogin.Controls.Add(label2);
+            pnlLogin.Location = new Point(261, 149);
+            pnlLogin.Name = "pnlLogin";
+            pnlLogin.Size = new Size(395, 290);
+            pnlLogin.TabIndex = 18;
+            pnlLogin.Paint += pnlLogin_Paint_1;
+            // 
+            // btnRegister
+            // 
+            btnRegister.BackColor = Color.Gray;
+            btnRegister.FlatStyle = FlatStyle.Flat;
+            btnRegister.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRegister.ForeColor = Color.Aqua;
+            btnRegister.Location = new Point(206, 219);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(148, 41);
+            btnRegister.TabIndex = 11;
+            btnRegister.TabStop = false;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.Gray;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.ForeColor = Color.Aqua;
+            btnLogin.Location = new Point(40, 219);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(148, 41);
+            btnLogin.TabIndex = 10;
+            btnLogin.TabStop = false;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            btnLogin.MouseEnter += btnLogin_MouseEnter;
+            btnLogin.MouseLeave += btnLogin_MouseLeave;
+            // 
+            // checkShowpassword
+            // 
+            checkShowpassword.AutoSize = true;
+            checkShowpassword.BackColor = Color.Transparent;
+            checkShowpassword.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkShowpassword.ForeColor = SystemColors.ControlLightLight;
+            checkShowpassword.Location = new Point(227, 176);
+            checkShowpassword.Name = "checkShowpassword";
+            checkShowpassword.Size = new Size(136, 24);
+            checkShowpassword.TabIndex = 9;
+            checkShowpassword.Text = "Show password";
+            checkShowpassword.UseVisualStyleBackColor = false;
+            checkShowpassword.CheckedChanged += checkShowpw_CheckedChanged;
+            // 
+            // txtPassword
+            // 
+            txtPassword.BackColor = Color.Bisque;
+            txtPassword.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPassword.Location = new Point(70, 136);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderText = "Password";
+            txtPassword.Size = new Size(282, 26);
+            txtPassword.TabIndex = 6;
+            txtPassword.TabStop = false;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.Image = Properties.Resources.pw_icon;
+            pictureBox4.Location = new Point(30, 132);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(34, 29);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.SlateBlue;
+            panel3.Location = new Point(30, 167);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(333, 3);
+            panel3.TabIndex = 4;
+            // 
+            // txtUsername
+            // 
+            txtUsername.BackColor = Color.Bisque;
+            txtUsername.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsername.Location = new Point(70, 82);
+            txtUsername.Name = "txtUsername";
+            txtUsername.PlaceholderText = "Username";
+            txtUsername.Size = new Size(282, 26);
+            txtUsername.TabIndex = 3;
+            txtUsername.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox5.Image = Properties.Resources._9187532;
+            pictureBox5.Location = new Point(30, 78);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(34, 29);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 2;
+            pictureBox5.TabStop = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.SlateBlue;
+            panel4.Location = new Point(30, 113);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(333, 3);
+            panel4.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Teal;
+            label2.Font = new Font("Castellar", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DarkRed;
+            label2.Location = new Point(52, 11);
+            label2.Name = "label2";
+            label2.Size = new Size(289, 34);
+            label2.TabIndex = 0;
+            label2.Text = "LAPTRINHMANG";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(979, 564);
+            Controls.Add(pnlLogin);
+            Controls.Add(pictureBox1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            Load += LoginForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            pnlLogin.ResumeLayout(false);
+            pnlLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Panel pnlLogin;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private CheckBox checkShowpassword;
+        private TextBox txtPassword;
+        private Panel panel3;
+        private TextBox txtUsername;
+        private Panel panel4;
+        private Label label2;
+        private Button btnLogin;
+        private Button btnRegister;
     }
 }
--- BattleGame.Client/Forms/MenuForm.Designer.cs
+++ BattleGame.Client/Forms/MenuForm.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MenuForm";
+            pictureBox1 = new PictureBox();
+            btnPlay = new Button();
+            btnLogout = new Button();
+            btnExit = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Gemini_Generated_Image_141glb141glb141g;
+            pictureBox1.Location = new Point(-59, -73);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1072, 638);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // btnPlay
+            // 
+            btnPlay.BackColor = Color.Transparent;
+            btnPlay.Font = new Font("Stencil", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPlay.ForeColor = Color.Fuchsia;
+            btnPlay.Image = Properties.Resources.fr;
+            btnPlay.Location = new Point(312, 185);
+            btnPlay.Name = "btnPlay";
+            btnPlay.Size = new Size(198, 52);
+            btnPlay.TabIndex = 1;
+            btnPlay.Text = "Play";
+            btnPlay.UseVisualStyleBackColor = false;
+            btnPlay.Click += btnPlay_Click;
+            btnPlay.MouseEnter += btnPlay_MouseEnter;
+            btnPlay.MouseLeave += btnPlay_MouseLeave;
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.Transparent;
+            btnLogout.Font = new Font("Stencil", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLogout.ForeColor = Color.Fuchsia;
+            btnLogout.Image = Properties.Resources.fr;
+            btnLogout.Location = new Point(312, 288);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(198, 51);
+            btnLogout.TabIndex = 2;
+            btnLogout.Text = "Log out";
+            btnLogout.UseVisualStyleBackColor = false;
+            btnLogout.Click += btnLogout_Click;
+            btnLogout.MouseEnter += btnLogout_MouseEnter;
+            btnLogout.MouseLeave += btnLogout_MouseLeave;
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = Color.Transparent;
+            btnExit.Font = new Font("Stencil", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnExit.ForeColor = Color.Fuchsia;
+            btnExit.Image = Properties.Resources.fr;
+            btnExit.Location = new Point(312, 388);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(198, 51);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = false;
+            btnExit.Click += btnExit_Click;
+            btnExit.MouseEnter += btnExit_MouseEnter;
+            btnExit.MouseLeave += btnExit_MouseLeave;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.SteelBlue;
+            label1.FlatStyle = FlatStyle.Popup;
+            label1.Font = new Font("Castellar", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.LightGreen;
+            label1.Image = Properties.Resources.Screenshot_2026_03_19_084759;
+            label1.Location = new Point(257, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(334, 73);
+            label1.TabIndex = 4;
+            label1.Text = "LTMang";
+            // 
+            // MenuForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(853, 530);
+            Controls.Add(label1);
+            Controls.Add(btnExit);
+            Controls.Add(btnLogout);
+            Controls.Add(btnPlay);
+            Controls.Add(pictureBox1);
+            Name = "MenuForm";
+            Text = "MenuForm";
+            Load += MenuForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnPlay;
+        private Button btnLogout;
+        private Button btnExit;
+        private Label label1;
     }
 }
