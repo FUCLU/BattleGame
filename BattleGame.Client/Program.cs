394230112e008352084ecf2d3d91e724afb489dc
Nguyen Tan Phat <tanphatnguyen.tp@gmail.com>
2026-05-23 16:21:41 +0700

    sua progr

--- BattleGame.Client/Program.cs
+++ BattleGame.Client/Program.cs
@@ -29,7 +29,7 @@ namespace BattleGame.Client
                 // return;  
             }
 
-            Application.Run(new CharacterSelection());
+            Application.Run(new DungeonMode());
         }
     }
 }
